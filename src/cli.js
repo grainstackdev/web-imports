3f62a4f3f106a957a7484185c37a7c6e101e7e5d
grainstackdev <99065516+grainstackdev@users.noreply.github.com>
2023-02-24 07:17:14 -0800

    chalk

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ import fs from 'fs'
 import path from 'path'
 import glob from "glob"
 import minimist from "minimist"
+import chalk from 'chalk'
 import {transformImports} from './index.js'
 
 const args = minimist(process.argv.slice(2))
@@ -13,7 +14,7 @@ const prefix = args['prefix'] || '/node_modules/'
 glob(g, {}, async (err, files) => {
   for (const file of files) {
     const filepath = path.resolve(file)
-    console.log("[web-imports]", file)
+    console.log(chalk.cyan("[web-imports]"), file)
     if (!fs.lstatSync(filepath).isDirectory()) {
       const str = fs.readFileSync(filepath, { encoding: "utf8" })
       const out = await transformImports(str, filepath, prefix)
